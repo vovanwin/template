6712f9f8f583303e9cb46f4be25604a186ca05b9
vovanwin <vkozind@ozon.ru>
2025-08-29 23:30:19 +0300

    fix

--- app/config/config.go
+++ app/config/config.go
@@ -1,14 +1,16 @@
 package config
 
 import (
-	"app/pkg/validator"
 	"fmt"
-	"github.com/ilyakaznacheev/cleanenv"
 	"log/slog"
 	"net"
 	"os"
 	"path"
 	"time"
+
+	"app/pkg/validator"
+
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 var configPath = "config/config.yml"
@@ -47,35 +49,35 @@ type (
 	}
 
 	Server struct {
-		Host string `env-required:"true" yaml:"host" env:"HOST" validate:"required"`
-		Port string `env-required:"true" yaml:"port" env:"PORT" validate:"required"`
-		Env  string `env-required:"true" yaml:"env" env:"ENV" validate:"required,oneof=local dev prod"`
+		Host string `env-required:"true" yaml:"host" env:"APP_HOST" validate:"required"`
+		Port string `env-required:"true" yaml:"port" env:"APP_PORT" validate:"required"`
+		Env  string `env-required:"true" yaml:"env" env:"APP_ENV" validate:"required,oneof=local dev prod"`
 
-		ContextTimeout    time.Duration `env-required:"true" yaml:"context_timeout" env:"CONTEXT_TIMEOUT" validate:"required"`
-		ReadHeaderTimeout time.Duration `env-required:"true" yaml:"read_header_timeout" env:"READ_HEADER_TIMEOUT" default:"60s"`
-		GracefulTimeout   time.Duration `env-required:"true" yaml:"grace_ful_timeout" env:"GRACE_FUL_TIMEOUT" default:"8s"`
+		ContextTimeout    time.Duration `env-required:"true" yaml:"context_timeout" env:"APP_CONTEXT_TIMEOUT" validate:"required"`
+		ReadHeaderTimeout time.Duration `env-required:"true" yaml:"read_header_timeout" env:"APP_READ_HEADER_TIMEOUT" default:"60s"`
+		GracefulTimeout   time.Duration `env-required:"true" yaml:"grace_ful_timeout" env:"APP_GRACE_FUL_TIMEOUT" default:"8s"`
 	}
 
 	Log struct {
-		Level string `env-required:"true" yaml:"level" env:"LOG_LEVEL"  validate:"required,oneof=DEBUG INFO WARN ERROR"`
+		Level string `env-required:"true" yaml:"level" env:"APP_LOG_LEVEL"  validate:"required,oneof=DEBUG INFO WARN ERROR"`
 	}
 
 	PG struct {
-		HostPG     string `env-required:"true" yaml:"host"          env:"HOST_PG" validate:"required"`
-		PortPG     string `env-required:"true" yaml:"port"          env:"PORT_PG" validate:"required"`
-		UserPG     string `env-required:"true" yaml:"user"          env:"USER_PG" validate:"required"`
-		PasswordPG string `env-required:"true" yaml:"password"      env:"PASSWORD_PG" validate:"required"`
-		SchemePG   string `env-required:"true" yaml:"scheme"        env:"SCHEME_PG" validate:"required"`
-		DbNamePG   string `env-required:"true" yaml:"db"            env:"DBNAME_PG" validate:"required"`
+		HostPG     string `env-required:"true" yaml:"host"          env:"APP_HOST_PG" validate:"required"`
+		PortPG     string `env-required:"true" yaml:"port"          env:"APP_PORT_PG" validate:"required"`
+		UserPG     string `env-required:"true" yaml:"user"          env:"APP_USER_PG" validate:"required"`
+		PasswordPG string `env-required:"true" yaml:"password"      env:"APP_PASSWORD_PG" validate:"required"`
+		SchemePG   string `env-required:"true" yaml:"scheme"        env:"APP_SCHEME_PG" validate:"required"`
+		DbNamePG   string `env-required:"true" yaml:"db"            env:"APP_DBNAME_PG" validate:"required"`
 	}
 	Rabbit struct {
-		URI string `env-required:"true" yaml:"amqp_url" env:"AMQP_URI" validate:"required"`
+		URI string `env-required:"true" yaml:"amqp_url" env:"APP_AMQP_URI" validate:"required"`
 	}
 
 	JWT struct {
-		SignKey    string        `env-required:"true" yaml:"sign_key" env:"SIGN_KEY" validate:"required"`
-		TokenTTL   time.Duration `env-required:"true" yaml:"token_ttl" env:"TOKEN_TTL" validate:"required"`
-		RefreshTTL time.Duration `env-required:"true" yaml:"refresh_token_ttl" env:"REFRESH_TOKEN_TTL" validate:"required"`
+		SignKey    string        `env-required:"true" yaml:"sign_key" env:"APP_SIGN_KEY" validate:"required"`
+		TokenTTL   time.Duration `env-required:"true" yaml:"token_ttl" env:"APP_TOKEN_TTL" validate:"required"`
+		RefreshTTL time.Duration `env-required:"true" yaml:"refresh_token_ttl" env:"APP_REFRESH_TOKEN_TTL" validate:"required"`
 	}
 )
 
