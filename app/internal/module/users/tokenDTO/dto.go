4e53c877a22603e913b58d1e7f6963635e4c3d8b
vovanwin <vkozind@ozon.ru>
2025-09-06 01:29:41 +0300

    fix

--- app/internal/module/users/tokenDTO/dto.go
+++ app/internal/module/users/tokenDTO/dto.go
@@ -1,19 +1,19 @@
 package tokenDTO
 
 import (
-	"app/internal/shared/types"
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
 type TokenClaims struct {
 	jwt.RegisteredClaims
-	UserId   types.UserID   `json:"user_id"`
-	RoleId   int            `json:"role_id"`
-	TenantId types.TenantID `json:"tenant_id"`
+	UserId   uuid.UUID `json:"user_id"`
+	RoleId   int       `json:"role_id"`
+	TenantId uuid.UUID `json:"tenant_id"`
 }
 
 type RefreshTokenClaims struct {
 	jwt.RegisteredClaims
-	UserId    types.UserID `json:"user_id"`
-	IsRefresh bool         `json:"is_refresh"` //TODO: рефреш токен заглушка до получаения FSSO
+	UserId    uuid.UUID `json:"user_id"`
+	IsRefresh bool      `json:"is_refresh"` //TODO: рефреш токен заглушка до получаения FSSO
 }
